8c4261722536e81d3315863a7673ac3950968fa2
Muhammad Zulkifli <zulkiflimuhammad771@gmail.com>
2025-07-07 12:27:42 +0700

    portfolio

---
+++ index.js
@@ -0,0 +1,15 @@
+window.onscroll = function() {
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+    
+    window.pageYOffset > fixedNav ? header.classList.add('navbar-fixed') : header.classList.remove('navbar-fixed'); 
+}
+
+// Hamburger
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
+
+hamburger.addEventListener('click', function() {
+    hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('hidden');
+});
